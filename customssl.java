12e21cb27b03bbb80bae0c435aa4fe22e630a59d
Admin <42235970+zencod-admin@users.noreply.github.com>
2025-10-28 11:10:26 -0400

    Update customssl.java

--- customssl.java
+++ customssl.java
@@ -1,3 +1,18 @@
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.security.cert.X509Certificate;
+
+
 @Configuration
 public class RestTemplateConfig {
 
